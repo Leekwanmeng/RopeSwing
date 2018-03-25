645d2f5c93dbed2bf1752429ffe1812d04b2267d
leekwanmeng <kwanmeng_lee@mymail.sutd.edu.sg>
2018-03-25 17:35:55 +0800

    Fixed Networking Capabilities

--- Assets/Scripts/RopeController.cs
+++ Assets/Scripts/RopeController.cs
@@ -5,31 +5,46 @@ using UnityEngine;
 public class RopeController : MonoBehaviour {
 
 	/*Public Fields*/
-	public GameObject ropeShooter;
 	public LineRenderer lineRenderer;
+	public bool ropeActive;
 
 	/*Private Fields*/
 	private SpringJoint2D rope;
+
+	void Start() {
+		lineRenderer = GetComponent<LineRenderer>();
+	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonDown(0)) {
-			ShootRope();
+			if (!ropeActive) {
+				ShootRope();
+			} else {
+				DestroyRope();
+			}
 		}
 	}
 
+	// Update per physics frame
 	void FixedUpdate() {
 	}
+
+	// Last update
+	void LateUpdate() {
+		RenderRope();
+	}
+
 
 	/*
 	* Updates after all update functions called
 	* Adds LineRenderer to existing rope (from player to anchor)
 	*/
-	void LateUpdate() {
+	void RenderRope() {
 		if (rope != null) {
 			lineRenderer.enabled = true;
 			lineRenderer.positionCount = 2;
-			lineRenderer.SetPosition(0, ropeShooter.transform.position);
+			lineRenderer.SetPosition(0, gameObject.transform.position);
 			lineRenderer.SetPosition(1, rope.connectedAnchor);
 		} else {
 			lineRenderer.enabled = false;
@@ -43,7 +58,7 @@ public class RopeController : MonoBehaviour {
 	*/
 	void ShootRope() {
 		Vector2 mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-		Vector2 position = ropeShooter.transform.position;
+		Vector2 position = gameObject.transform.position;
 		Vector2 direction = mousePosition - position;
 
 		RaycastHit2D hit = Physics2D.Raycast (position, direction, 
@@ -53,18 +68,23 @@ public class RopeController : MonoBehaviour {
 
 		if (hit.collider != null) {
 			print("fired");
-			SpringJoint2D newRope = ropeShooter.AddComponent<SpringJoint2D>();
-			newRope.enableCollision = false;
-			newRope.frequency = 0.5f;
-			newRope.connectedAnchor = hit.point;
-			newRope.enabled = true;
-
-			GameObject.DestroyImmediate(rope);
-			rope = newRope;
+			rope = gameObject.AddComponent<SpringJoint2D>();
+			rope.enableCollision = false;
+			rope.frequency = 0.5f;
+			rope.connectedAnchor = hit.point;
+			rope.enabled = true;
+			ropeActive = true;
 		}
-
-
-
-
 	}
+
+
+	/*
+	* Destroys rope if exists
+	*/
+	void DestroyRope() {
+		GameObject.DestroyImmediate(rope);
+		ropeActive = false;
+	}
+
+
 }
