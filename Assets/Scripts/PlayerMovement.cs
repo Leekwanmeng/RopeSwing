9a29a3649b086b8300a8346a4558e13f8f023f53
leekwanmeng <kwanmeng_lee@mymail.sutd.edu.sg>
2018-03-27 18:35:34 +0800

    Added follow cam (to fix)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,21 +8,27 @@ public class PlayerMovement : MonoBehaviour {
 	public float forceToAdd = 20;
 	public float tiltThreshold = 0.4f;
 
+	/*Private fields*/
+	private RopeController ropeController;
+
 	void Start () {
-		//gives it force
+		//gives it upward force
 		GetComponent<Rigidbody2D>().velocity = Vector2.up * 5;
+		ropeController = gameObject.GetComponent<RopeController>();
 	}
 
 
 	void Update () {
-		RopeController ropeController = gameObject.GetComponent<RopeController>();
 		if (ropeController.ropeActive) {
 			tiltForce();
 		}
 	}
 
+	/*
+	* Uses the mobile's gyroscope to detect tilting
+	* Adds force to player if sufficient tilt AND player is on the rope
+	*/
 	void tiltForce() {
-
 		if (Input.acceleration.x > tiltThreshold) {
 			GetComponent<Rigidbody2D>().AddForce(Vector2.right*forceToAdd);
 		}
---
+++ Assets/Scripts/SmoothCamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCamera : MonoBehaviour {
+
+	public float interpVelocity;
+	public float minDistance;
+	public float followDistance;
+	public GameObject target;
+	public Vector3 offset;
+	Vector3 targetPos;
+	// Use this for initialization
+	void Start () {
+		targetPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (target)
+		{
+			Vector3 posNoZ = transform.position;
+			posNoZ.z = target.transform.position.z;
+
+			Vector3 targetDirection = (target.transform.position - posNoZ);
+
+			interpVelocity = targetDirection.magnitude * 5f;
+
+			targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime); 
+
+			transform.position = Vector3.Lerp( transform.position, targetPos + offset, 0.25f);
+
+		}
+	}
+}
