be7eaccff3de733ffe918f64a48af7d7c9d9a349
SNZB <32621382+SNZB@users.noreply.github.com>
2018-04-04 03:37:01 +0800

    Add files via upload
    testing about rope

---
+++ RobeTest1.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class RobeTest1 {
+
+	[Test]
+	public void RobeTest1SimplePasses() {
+		// Use the Assert class to test conditions.
+	}
+	[Test]
+	public void RopeActiveInitTest(){
+		
+		RopeController Rope = new RopeController();
+
+		Assert.AreEqual(false, Rope.getRA());
+
+	}
+	[Test]
+	public void RopeActiveSetRATest(){
+
+		RopeController Rope = new RopeController();
+		Rope.setRA (true);
+
+		Assert.AreEqual(true, Rope.getRA());
+
+	}
+
+	//ask how can i connect rope to the wall
+	[UnityTest]
+	public IEnumerator SRTEST1()
+	{
+		GameObject rope = new GameObject();
+		RopeController r = new RopeController();
+		rope.AddComponent(typeof(RopeController));
+		rope.AddComponent (typeof(DistanceJoint2D));
+
+
+		RopeController ropeC = rope.GetComponent<RopeController>();
+//		ropeC.ropeActive = true;
+		ropeC.ShootRopeBypassVectors();
+
+
+		Assert.AreEqual(true, ropeC.ropeActive);
+
+
+		yield return null;
+
+	}
+	[UnityTest]
+	public IEnumerator SRTEST2()
+	{
+		GameObject rope = new GameObject();
+		RopeController r = new RopeController();
+		rope.AddComponent(typeof(RopeController));
+		rope.AddComponent (typeof(DistanceJoint2D));
+
+
+		RopeController ropeC = rope.GetComponent<RopeController>();
+	    ropeC.ropeActive = false;
+		ropeC.ShootRopeBypassHit();
+
+
+		Assert.AreEqual(true, ropeC.ropeActive);
+
+
+		yield return null;
+
+	}
+	[UnityTest]
+	public IEnumerator RRTEST1()
+	{   
+		GameObject rope = new GameObject();
+		RopeController r = new RopeController();
+		rope.AddComponent(typeof(RopeController));
+		rope.AddComponent (typeof(LineRenderer));
+
+		RopeController ropeC = rope.GetComponent<RopeController>();
+		ropeC.ropeActive = false;
+		ropeC.setRope ();
+		ropeC.RenderRopeT();
+
+
+		Assert.AreEqual(true, ropeC.ropeActive);
+
+
+		yield return null;
+
+	}
+	[UnityTest]
+	public IEnumerator RRTEST2()
+	{   
+		GameObject rope = new GameObject();
+		RopeController r = new RopeController();
+		rope.AddComponent(typeof(RopeController));
+		rope.AddComponent (typeof(LineRenderer));
+
+		RopeController ropeC = rope.GetComponent<RopeController>();
+		ropeC.ropeActive = true;
+		ropeC.RenderRopeT();
+
+
+		Assert.AreEqual(false, ropeC.ropeActive);
+
+
+		yield return null;
+
+	}
+
+
+
+	// A UnityTest behaves like a coroutine in PlayMode
+	// and allows you to yield null to skip a frame in EditMode
+	[UnityTest]
+	public IEnumerator RobeTest1WithEnumeratorPasses() {
+		// Use the Assert class to test conditions.
+		// yield to skip a frame
+		yield return null;
+	}
+}
---
+++ RopeController.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class RopeController : NetworkBehaviour {
+
+	/*Public Fields*/
+	public LineRenderer lineRenderer;
+	public bool ropeActive;
+
+	/*Private Fields*/
+	private Animator animator;
+	private DistanceJoint2D rope;
+	private Vector2 touchPosition;
+
+	public override void OnStartLocalPlayer() {
+         Camera.main.GetComponent<SmoothCamera>().setPlayer(gameObject);
+    }
+
+	void Start() {
+		lineRenderer = GetComponent<LineRenderer>();
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isLocalPlayer) {
+			return;
+		}
+		TouchDetection();
+	}
+
+	// Update per physics frame
+	void FixedUpdate() {
+	}
+
+	// Last update
+	void LateUpdate() {
+		RenderRope();
+	}
+
+
+	/*
+	* Updates after all update functions called
+	* Adds LineRenderer to existing rope (from player to anchor)
+	*/
+	void RenderRope() {
+		if (rope != null) {
+			lineRenderer.enabled = true;
+			lineRenderer.positionCount = 2;
+			lineRenderer.SetPosition(0, gameObject.transform.position);
+			lineRenderer.SetPosition(1, rope.connectedAnchor);
+			animator.SetBool("ropeActive", true);
+		} else {
+			lineRenderer.enabled = false;
+			animator.SetBool("ropeActive", false);
+		}
+	}
+
+
+	/*
+	* Detects Touches on mobile phone
+	* Creates rope on touch if no rope exists, else removes current rope
+	*/
+	void TouchDetection() {
+		if (Input.touchCount > 0) {
+			Touch touch = Input.GetTouch(0);
+			if (touch.phase == TouchPhase.Began ||
+				touch.phase == TouchPhase.Stationary ||
+				touch.phase == TouchPhase.Moved) {
+
+				touchPosition = Camera.main.ScreenToWorldPoint
+				(new Vector2(touch.position.x, touch.position.y));
+
+
+			} else if (touch.phase == TouchPhase.Ended) {
+				if (!ropeActive) {
+					touchPosition = Camera.main.ScreenToWorldPoint
+					(new Vector2(touch.position.x, touch.position.y));
+					ShootRope(touchPosition);
+				} else {
+					DestroyRope();
+				}
+			}
+		}
+	}
+
+
+	/*
+	* Raycasts to clicked position if it collides with a wall
+	* Adds new rope if successful while deleting previous rope
+	* 
+	* @param Player touch position on mobile screen
+	*/
+	void ShootRope(Vector2 touchPosition) {
+		// Vector2 mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		Vector2 playerPosition = gameObject.transform.position;
+		Vector2 direction = touchPosition - playerPosition;
+
+		RaycastHit2D hit = Physics2D.Raycast (playerPosition, direction, 
+							Mathf.Infinity, 1 << LayerMask.NameToLayer("Wall"));
+
+		if (hit.collider != null) {
+			rope = gameObject.AddComponent<DistanceJoint2D>();
+			rope.enableCollision = true;
+			rope.distance = hit.distance;
+			rope.connectedAnchor = hit.point;
+			rope.enabled = true;
+			ropeActive = true;
+		}
+	}
+
+
+	/*
+	* Destroys rope if exists
+	*/
+	void DestroyRope() {
+		GameObject.Destroy(rope);
+		ropeActive = false;
+	
+	}
+
+
+
+	/*
+	 * -------for testing purpose-----
+	 */
+
+
+
+	public void setRA(bool RA){
+		ropeActive = RA;
+	}
+	public bool getRA(){
+		return ropeActive;
+	}
+	public DistanceJoint2D getRope(){
+		return rope;
+	}
+	public void setRope(){
+		rope = gameObject.AddComponent<DistanceJoint2D>();
+	}
+
+	public void ShootRopeBypassVectors() {
+		Vector2 playerPosition = new Vector2(3,3);
+		Vector2 direction = Vector2.down;
+
+		RaycastHit2D hit = Physics2D.Raycast (playerPosition, direction, 
+			Mathf.Infinity, 1 << LayerMask.NameToLayer("Wall"));
+
+		if (hit.collider != null) {
+			rope = gameObject.AddComponent<DistanceJoint2D>();
+			rope.enableCollision = true;
+			rope.distance = hit.distance;
+			rope.connectedAnchor = hit.point;
+			rope.enabled = true;
+			ropeActive = true;
+		}
+	}
+	public void ShootRopeBypassHit() {
+		
+		Vector2 playerPosition = new Vector2(3,3);
+		Vector2 direction = Vector2.down;
+
+			rope = gameObject.AddComponent<DistanceJoint2D>();
+			rope.enableCollision = true;
+			rope.enabled = true;
+			ropeActive = true;
+
+	}
+	public void RenderRopeT() {
+		lineRenderer = GetComponent<LineRenderer>();
+		if (rope != null) {
+			lineRenderer.enabled = true;
+			lineRenderer.positionCount = 2;
+			lineRenderer.SetPosition(0, gameObject.transform.position);
+			lineRenderer.SetPosition(1, rope.connectedAnchor);
+			setRA(true);
+		} else {
+			lineRenderer.enabled = false;
+			setRA (false);
+		}
+	}
+
+
+}
