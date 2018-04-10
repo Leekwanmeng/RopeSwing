4603c9192957d3f51fe12bd5e7b59753b06f5efd
EdwinChenXM <edc@berkeley.edu>
2018-04-10 23:17:33 +0800

    added trigger and moving platform(no artworks)

---
+++ Assets/Scripts/PlatformMove/Trigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour {
+
+	
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+            System.Console.Write("We are triggering the event");
+            GameObject.Find("Moving Platform").GetComponent<PlatformController>().triggered = true;
+
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+            GameObject.Find("Moving Platform").GetComponent<PlatformController>().triggered = false;
+
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+    }
+
+
+
+}
